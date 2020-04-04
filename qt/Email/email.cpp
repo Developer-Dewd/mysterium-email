028834affcbcb53cbde9d7cc2ec6528c540028fa
coderrick <derrick.duncan@temple.edu>
2020-04-04 18:44:16 -0400

    Added subject field and layout formatting

--- qt/Email/email.cpp
+++ qt/Email/email.cpp
@@ -25,17 +25,14 @@ Email::~Email()
 
 void Email::on_SendButton_clicked()
 {
-    //string testmsg = "Are you ready to send you email? \nsize: 47KB cost: 00.34 MYST";
+    //string testmsg = "Are you ready to send you email? \nsize: 47KB cost: 00.34 MYST";//string won't work as message param
     QMessageBox::StandardButton reply = QMessageBox::question(this, "Save Draft", "Are you ready to send you email? \nSIZE: 47KB COST: 00.34 MYST", QMessageBox::Yes | QMessageBox::No);
     if(reply == QMessageBox::Yes){
-        //TODO: Save draft code, Mysterium proxy
-        ui->label->setText("Draft Saved!");
+        //TODO: Smart contract, Mysterium proxy
+        ui->label->setText("Email Sent!");
     }else{
-        qDebug() << "Save Draft Cancelled, No was clicked";
+        qDebug() << "Email Cancelled, No was clicked";
     }
-    //ui->label->setText("Email Sent!");
-    //TODO: Send email code
-    //this->close();
 }
 
 void Email::on_SaveDraftButton_clicked()
