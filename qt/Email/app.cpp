954e03b5142e47c15d9f9cefed7731a885efa7f1
coderrick <derrick.duncan@temple.edu>
2020-04-07 08:19:28 -0400

    changes to populate emails in QListWidget

--- qt/Email/app.cpp
+++ qt/Email/app.cpp
@@ -8,9 +8,13 @@ App::App(QWidget *parent) :
     ui(new Ui::App)
 {
     ui->setupUi(this);
-    //QListWidgetItem i = new QListWidgetItem(QIcon(":q/resources/img/email.png"), "@0x53f3422 \nSubject Hello World!");
-    //QListWidgetItem *item = new QListWidgetItem(QIcon(""));
 
+    //Populate the list view with
+    foreach(QString reqs, reqlist.get()){
+       ui->InboxListWidget->addItem(reqs.getURLPath());
+    }
+
+    //Placeholders
     ui->InboxListWidget->addItem(new QListWidgetItem(QIcon(":q/resources/img/email.png"),"@0x53f3422 \nSubject Hello World!"));
     ui->InboxListWidget->addItem(new QListWidgetItem(QIcon(":q/resources/img/email.png"),"@0x53f3422 \nSubject Hello World!"));
     ui->InboxListWidget->addItem(new QListWidgetItem(QIcon(":q/resources/img/email.png"),"@0x932h23h \nHello team I created.."));
