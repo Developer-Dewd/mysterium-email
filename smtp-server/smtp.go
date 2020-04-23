75b28c83d4dea894be02ac46993944f22fdd0daf
coderrick <derrick.duncan@temple.edu>
2020-04-23 01:23:04 -0400

    Added send route and test functions

--- smtp-server/smtp.go
+++ smtp-server/smtp.go
@@ -1,5 +1,46 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+	"net/smtp"
+)
 
+func main() {
+	IOtest()
+	Logtest()
+	Smtptest()
+	startServer()
+}
+
+func iotest() {
+	fmt.Println("smtp serve")
+}
+
+func logtest() {
+	log.Println("smtp server executed")
+}
+
+func smtptest() {
+	hostURL := "smtp.gmail.com"
+	hostPort := "587"
+	sender := "wildun.dwd@gmail.com"
+	reciever := "dvdwd.io@gmail.com"
+	//authentication
+	auth := smtp.PlainAuth(
+		"",
+		sender,
+		password,
+		hostURL,
+	)
+}
+
+func startServer() {
+	http.HandleFunc("/send", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+	fmt.Println("Listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
